e29a5513391e088ac482eeda9146eadf60b652d2
rizkajuliant20 <rizkariz20@gmail.com>
2022-07-21 23:24:15 +0700

    fix: remove unused code

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,28 +2,28 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import { useNavigate } from "react-router-dom";
-import { auth } from '../config/firebase';
-import useAuth from '../hooks/useAuth';
+import useAuth from "../hooks/useAuth";
 
-const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/original'
+const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
 export default function MovieCard({ movie }) {
   const { user } = useAuth();
-  const isLogin = user ? true : false
-  
+  const isLogin = user ? true : false;
+
   let navigate = useNavigate();
   const onClickHandler = (movie) => {
     // console.log(movie);
     isLogin && navigate(`/movie-detail/${movie.id}`);
-  }
+  };
 
   return (
-    <Card id={movie.id} sx={{ display: "flex", mt: 5}}>
+    <Card id={movie.id} sx={{ display: "flex", mt: 5 }}>
       <CardMedia
         component="img"
         sx={{ width: 150, height: 225 }}
         image={BASE_IMAGE_URL + movie.poster_path}
-        alt="movie.jpg" onClick={()=>onClickHandler(movie)}
+        alt="movie.jpg"
+        onClick={() => onClickHandler(movie)}
       />
     </Card>
   );
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -8,39 +8,56 @@ const MovieDetails = () => {
 
   const [movieDetail, setMovieDetail] = useState([]);
 
-  useEffect(() =>{
-      const fetchData = async () => {
-          const response = await axios.get(`https://api.themoviedb.org/3/movie/${params?.id}?api_key=ccd631bba7a9cee1d6a100b57551aeab&language=en-US`);
-          console.log("ini response: ", response.data)
-          setMovieDetail(response.data);
-      }
-      fetchData();
-  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${params?.id}?api_key=ccd631bba7a9cee1d6a100b57551aeab&language=en-US`
+      );
+      console.log("ini response: ", response.data);
+      setMovieDetail(response.data);
+    };
+    fetchData();
+  }, [params?.id]);
 
   return (
     <>
-      <Box sx={{ backgroundColor: "white", width: '100%', height: 500, display: "flex", alignItems: "center" }}>
-      {/* backgroundColor: "cyan", width: '100%', height: 500, display: "flex", alignItems: "center" */}
+      <Box
+        sx={{
+          backgroundColor: "white",
+          width: "100%",
+          height: 500,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        {/* backgroundColor: "cyan", width: '100%', height: 500, display: "flex", alignItems: "center" */}
         {/*  display: "flex", alignItems: "center", backgroundSize: 'cover', height: 500, backgroundImage: `url(https://image.tmdb.org/t/p/original/${movieDetail.backdrop_path})` */}
-        <Box sx={{ ml: 5, mr: 5, width: '40%'}}>
-          <Typography><h3>{movieDetail.title}</h3></Typography>
-          <Typography sx={{ mb: 5}}>{movieDetail.overview}</Typography>
-          <Button sx={{ m: 1 }}  variant="contained">Play</Button>
-          <Button sx={{ m: 1 }} variant="outlined">Description</Button>
+        <Box sx={{ ml: 5, mr: 5, width: "40%" }}>
+          <Typography>
+            <h3>{movieDetail.title}</h3>
+          </Typography>
+          <Typography sx={{ mb: 5 }}>{movieDetail.overview}</Typography>
+          <Button sx={{ m: 1 }} variant="contained">
+            Play
+          </Button>
+          <Button sx={{ m: 1 }} variant="outlined">
+            Description
+          </Button>
         </Box>
         <CardMedia
           component="img"
-          sx={{ width: '100%', height: 500 }}
+          sx={{ width: "100%", height: 500 }}
           image={`https://image.tmdb.org/t/p/original/${movieDetail.backdrop_path}`}
           alt="movie.jpg"
         />
-        {console.log("ini movie detail: ", movieDetail) }
-        
+        {console.log("ini movie detail: ", movieDetail)}
       </Box>
-      <Divider/>
+      <Divider />
       <Box sx={{ m: 5 }}>
-        <Typography >Description</Typography>
-        <Typography><h3>{movieDetail.title}</h3></Typography>
+        <Typography>Description</Typography>
+        <Typography>
+          <h3>{movieDetail.title}</h3>
+        </Typography>
         <Typography>{movieDetail.overview}</Typography>
       </Box>
     </>
